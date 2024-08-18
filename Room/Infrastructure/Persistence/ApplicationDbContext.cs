167d9163054d74383e02589f686ffd054f5761e9
Stepan <cntgf.2010@yandex.ru>
2024-08-18 20:22:36 +0300

    Инициализация карт при их отсутствии

--- Room/Infrastructure/Persistence/ApplicationDbContext.cs
+++ Room/Infrastructure/Persistence/ApplicationDbContext.cs
@@ -1,12 +1,57 @@
 ﻿using Domain.Interfaces;
 using Domain.Models;
 using Microsoft.EntityFrameworkCore;
+using Team_8.Contracts.Enums;
 
 namespace Infrastructure.Persistence;
 
-public class ApplicationDbContext : DbContext, IApplicationDbContext
+public sealed class ApplicationDbContext : DbContext, IApplicationDbContext
 {
-    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) {}
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+        if(Maps.Any(x => x.Name == "4x4")) return;
+        Maps.AddRange([
+            new Map
+            {
+                Id = 1,
+                Name = "4x4",
+                CountRow = 4,
+                CountColumn = 4,
+                TilesType =
+                [
+                    TilesType.Grass, TilesType.Grass, TilesType.Wall, TilesType.Grass,
+                    TilesType.Wall, TilesType.Grass, TilesType.Grass, TilesType.Grass,
+                    TilesType.Grass, TilesType.Grass, TilesType.Grass, TilesType.Wall,
+                    TilesType.Grass, TilesType.Wall, TilesType.Grass, TilesType.Grass
+                ]
+            },
+            new Map
+            {
+                Id = 2,
+                Name = "3x3CentralWall",
+                CountRow = 3,
+                CountColumn = 3,
+                TilesType =
+                [
+                    TilesType.Grass, TilesType.Grass, TilesType.Grass,
+                    TilesType.Grass, TilesType.Wall, TilesType.Grass,
+                    TilesType.Grass, TilesType.Grass, TilesType.Grass
+                ]
+            },
+            new Map
+            {
+                Id = 3,
+                Name = "2x2",
+                CountRow = 2,
+                CountColumn = 2,
+                TilesType =
+                [
+                    TilesType.Grass, TilesType.Grass, TilesType.Grass, TilesType.Grass
+                ]
+            }
+        ]);
+        SaveChanges();
+    }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
@@ -25,24 +70,24 @@ public class ApplicationDbContext : DbContext, IApplicationDbContext
     public DbSet<Room> Rooms => Set<Room>();
     public DbSet<UnitType> UnitTypes => Set<UnitType>();
     public DbSet<UserState> UserStates => Set<UserState>();
-    
-     public DbSet<T>? GetTable<T>() where T : class
-        {
-            var type = typeof(T);
-            
-            if(type == typeof(User))
-                return Users as DbSet<T>;
-            if(type == typeof(Map))
-                return Maps as DbSet<T>;
-            if(type == typeof(Room))
-                return Rooms as DbSet<T>;
-            if(type == typeof(UnitType))
-                return UnitTypes as DbSet<T>;
-            if(type == typeof(UserState))
-                return UserStates as DbSet<T>;
-            
-            return null;
-        }
+
+    public DbSet<T>? GetTable<T>() where T : class
+    {
+        var type = typeof(T);
+
+        if(type == typeof(User))
+            return Users as DbSet<T>;
+        if(type == typeof(Map))
+            return Maps as DbSet<T>;
+        if(type == typeof(Room))
+            return Rooms as DbSet<T>;
+        if(type == typeof(UnitType))
+            return UnitTypes as DbSet<T>;
+        if(type == typeof(UserState))
+            return UserStates as DbSet<T>;
+
+        return null;
+    }
 
     #endregion
 }
